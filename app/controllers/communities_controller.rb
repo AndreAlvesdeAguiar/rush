a8f7750cc0c5a8f28833ffca017e066e0111b83d
AndreAlvesdeAguiar <aguiar1001@hotmail.com>
2021-06-01 23:17:45 -0300

    complete part 1 but have any errors

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,10 +1,13 @@
 class CommunitiesController < ApplicationController
-  before_action :authenticate_account!, except: [ :index, :show]
+  before_action :authenticate_account!, except: [ :index, :show ]
+  before_action :set_post, only: [:show]
+  
   def index
     @communities = Community.all
   end
 
   def show
+    @posts = @community.posts
   end
   
   def new
@@ -13,5 +16,24 @@ class CommunitiesController < ApplicationController
 
 
   def create
+    @community = Community.new community_values
+    @community.account_id = current_account.id
+
+    if @community.save
+      redirect_to communities_path
+    else
+      render :new
   end
+end
+
+  private
+
+  def set_community
+    @community = Community.find(params[:id])
+  end
+
+  def community_values
+    params.require(:community).permit(:name, :url, :rules)
+  end
+
 end
